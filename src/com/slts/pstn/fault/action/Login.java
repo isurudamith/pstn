29412408ceb41e4a86c537f40f366e8fa7060400
isuru <idamithw@gmail.com>
2018-09-20 15:16:26 +0530

    R1000 || eclipse build with maven

---
+++ src/com/slts/pstn/fault/action/Login.java
@@ -0,0 +1,27 @@
+package com.slts.pstn.fault.action;
+
+import java.awt.PageAttributes.MediaType;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/login")
+public class Login {
+	
+//	@GET
+//	@Produces
+//	public String Log (MediaType) {
+//		String responses =null;
+//		return responses ;
+//	}
+//	
+//	@GET
+//	@Produces
+//	public String Log(MediaType) {
+//		String responses =null;
+//		return responses ;
+//	}
+
+
+}
