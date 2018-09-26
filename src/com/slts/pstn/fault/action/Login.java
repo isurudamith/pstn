591b7aee17811f6d0cbf8dad980c35cc7203a13c
isuru <idamithw@gmail.com>
2018-09-26 13:23:39 +0530

    R 1001 || Helo

--- src/com/slts/pstn/fault/action/Login.java
+++ src/com/slts/pstn/fault/action/Login.java
@@ -1,6 +1,6 @@
 package com.slts.pstn.fault.action;
-
-import java.awt.PageAttributes.MediaType;
+//com.slts.pstn.fault.action
+//import java.awt.PageAttributes.MediaType;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,16 +9,27 @@ import javax.ws.rs.Produces;
 @Path("/login")
 public class Login {
 	
-    @GET
-	@Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-	public String Log() {
-		String responses ="HELLO";
+  /*  @GET
+	@Produces(MediaType.APPLICATION_JSON_TYPE)
+    @Path("{name}")
+	public String Log(@Context HttpHeaders httpHeaders ,@PathParam("name") String name) {
+    	
+		String responses ="HELLO"+name;
 		return responses ;
-	}
+	}*/
+//	
+	
+	 @GET
+		@Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+		public String Log() {
+			String responses ="HELLO";
+			return responses ;
+		}
+	
 //	
 //	@GET
 //	@Produces
-//	public String Log(MediaType) {
+//	public String Log(MediaType.) {
 //		String responses =null;
 //		return responses ;
 //	}
